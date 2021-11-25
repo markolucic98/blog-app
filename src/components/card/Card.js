03d23d29912234992fbecc9f8f7db237e66fcf82
unknown <lucicmarko48@gmail.com>
2021-11-25 20:55:31 +0100

    Create App Component

---
+++ src/components/card/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Card.module.css";
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <h1>Card</h1>
+    </div>
+  );
+};
+
+export default Card;
