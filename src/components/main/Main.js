438c338be1456637c0e895a8c2350d53aa3b9a0e
unknown <lucicmarko48@gmail.com>
2021-11-30 20:15:10 +0100

    add Bootstrap to project

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,15 @@
-import Card from "../card/Card";
+import CardComponent from "../card/CardComponent";
 import React from "react";
 import styles from "./Main.module.css";
 const Main = () => {
   return (
     <div className={styles.main}>
       <h1>This is Main</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
+      <CardComponent />
     </div>
   );
 };
