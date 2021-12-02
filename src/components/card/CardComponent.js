e73436d84b57c6e5cd9071e2f1e7ad155e7ccc49
unknown <lucicmarko48@gmail.com>
2021-12-02 21:06:01 +0100

    add styles on Card Component

--- src/components/card/CardComponent.js
+++ src/components/card/CardComponent.js
@@ -2,16 +2,19 @@ import React from "react";
 import styles from "./Card.module.css";
 import Card from "react-bootstrap/Card";
 import Button from "@restart/ui/esm/Button";
-const CardComponent = () => {
+const CardComponent = ({ imgSrc, title, text }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+    <Card
+      style={{
+        width: "300px",
+        margin: "20px 0 20px 0",
+        backgroundColor: "#f4f4f4",
+      }}
+    >
+      <Card.Img variant="top" src={imgSrc} style={{ height: "200px" }} />
+      <Card.Body className={styles.Body}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text} </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
